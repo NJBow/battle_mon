1ad9aa7ef7690fd95dbee835034948fc3fd0abb8
NJBow <NJB@Nicks-MacBook-Pro-2.local>
2016-07-22 10:04:17 +0100

    graphics

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,8 @@
-require 'player'
+require_relative 'player'
 
 class Game
 
-attr_reader :player_1 :player_2
+attr_reader :player_1, :player_2
 
     def initialize(player_1, player_2)
       @player_1 = player_1
